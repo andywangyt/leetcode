0edac68347e1560e9e21a779adc332131af1fd29
Andrew <68841507+andywangyt@users.noreply.github.com>
2021-11-07 12:25:27 -0800

    Time: 0 ms (100.00%), Space: 38.4 MB (79.02%) - LeetHub

---
+++ search-a-2d-matrix/search-a-2d-matrix.java
@@ -0,0 +1,41 @@
+class Solution {
+	public static boolean searchMatrix(int[][] matrix, int target) {
+		int m = matrix.length;
+		if (m == 0)
+			return false;
+		int n = matrix[0].length;
+
+		// binary search
+		int left = 0, right = m * n - 1;
+		int pivotIdx, pivotElement;
+		while (left <= right) {
+			pivotIdx = (left + right) / 2;
+			pivotElement = matrix[pivotIdx / n][pivotIdx % n];
+			if (target == pivotElement)
+				return true;
+			else {
+				if (target < pivotElement)
+					right = pivotIdx - 1;
+				else
+					left = pivotIdx + 1;
+			}
+		}
+		return false;
+	}
+	
+    public static void main(String args[]) {
+
+       
+    	int[][] matrix = new int[][] {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
+    	
+//    	int target = 13;
+    	
+    	int target = 23;
+    	
+    	boolean isSearched = Solution.searchMatrix(matrix, target);
+    	
+    	System.out.println("isSearched " + isSearched);
+    	
+    
+    }
+}
