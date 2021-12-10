549a71d3157cdfe471898acd582b8e8686e9f1c5
Andrew <68841507+andywangyt@users.noreply.github.com>
2021-12-10 00:04:39 -0800

    Time: 10 ms (46.78%), Space: 39.4 MB (51.48%) - LeetHub

--- longest-substring-with-at-most-k-distinct-characters/longest-substring-with-at-most-k-distinct-characters.java
+++ longest-substring-with-at-most-k-distinct-characters/longest-substring-with-at-most-k-distinct-characters.java
@@ -1,4 +1,24 @@
 class Solution {
+    
+/**
+ * https://leetcode.com/problems/longest-substring-with-at-most-k-distinct-characters/
+ * 
+ * 
+    Pseudocode 
+    
+   1. Return 0 if the string is empty or k is equal to zero.
+   2. Set both set pointers in the beginning of the string left = 0 and right = 0 and
+    init max substring length max_len = 1.
+	While right pointer is less than N:
+	
+   3. Add the current character s[right] in the hashmap and move right pointer to the right.
+   4. If hashmap contains k + 1 distinct characters, remove the leftmost character from
+    the hashmap and move the left pointer so that sliding window contains again k distinct characters only.
+	Update max_len.
+ * 
+ * 
+ */
+    
     public static int lengthOfLongestSubstringKDistinct(String s, int k) {
         int n = s.length();
         if (n * k == 0) {
