196c881ad317535df96e3b156cb4134e9814f4d4
Andrew <68841507+andywangyt@users.noreply.github.com>
2021-12-15 23:55:18 -0800

    Time: 11 ms (5.21%), Space: 52 MB (6.43%) - LeetHub

--- missing-number/missing-number.java
+++ missing-number/missing-number.java
@@ -1,21 +1,26 @@
 class Solution {
     
-    //    public int missingNumber(int[] nums) {
-//        Set<Integer> numSet = new HashSet<Integer>();
-//        for (int num : nums) numSet.add(num);
-//
-//        int expectedNumCount = nums.length + 1;
-//        for (int number = 0; number < expectedNumCount; number++) {
-//            if (!numSet.contains(number)) {
-//                return number;
-//            }
-//        }
-//        return -1;
-//    }
-//
-//    
-    
-   /**
+	/*
+	 * This algorithm is almost identical to the brute force approach, except we
+	 * first insert each element of nums into a set, allowing us to later query for
+	 * containment in \mathcal{O}(1)O(1) time.
+	 */
+
+    public int missingNumber(int[] nums) {
+        Set<Integer> numSet = new HashSet<Integer>();
+        for (int num : nums) numSet.add(num);
+
+        int expectedNumCount = nums.length + 1;
+        for (int number = 0; number < expectedNumCount; number++) {
+            if (!numSet.contains(number)) {
+                return number;
+            }
+        }
+        return -1;
+    }
+  
+
+	/**
 	 * First, we sort nums. Then, we check the two special cases that can be handled
 	 * in constant time - ensuring that 0 is at the beginning and that nn is at the
 	 * end. Given that those assumptions hold, the missing number must somewhere
@@ -28,31 +33,29 @@ class Solution {
 	 * Thus, as soon as we find an unexpected number, we can simply return the
 	 * expected number.
 	 */
-    
-	public int missingNumber(int[] nums) {
-		Arrays.sort(nums);
-
-		// Ensure that n is at the last index
-		if (nums[nums.length - 1] != nums.length) {
-			return nums.length;
-		}
-		// Ensure that 0 is at the first index
-		else if (nums[0] != 0) {
-			return 0;
-		}
-
-		// If we get here, then the missing number is on the range (0, n)
-		for (int i = 1; i < nums.length; i++) {
-			int expectedNum = nums[i - 1] + 1;
-			if (nums[i] != expectedNum) {
-				return expectedNum;
-			}
-		}
-
-		// Array was not missing any numbers
-		return -1;
-	}
-        
+//	public int missingNumber(int[] nums) {
+//		Arrays.sort(nums);
+//
+//		// Ensure that n is at the last index
+//		if (nums[nums.length - 1] != nums.length) {
+//			return nums.length;
+//		}
+//		// Ensure that 0 is at the first index
+//		else if (nums[0] != 0) {
+//			return 0;
+//		}
+//
+//		// If we get here, then the missing number is on the range (0, n)
+//		for (int i = 1; i < nums.length; i++) {
+//			int expectedNum = nums[i - 1] + 1;
+//			if (nums[i] != expectedNum) {
+//				return expectedNum;
+//			}
+//		}
+//
+//		// Array was not missing any numbers
+//		return -1;
+//	}
         
    public static void main(String args[]) {
 		int[] s = new int[] { 3, 0, 1 };
