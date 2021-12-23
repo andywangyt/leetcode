27714613cd7949793c4ea521ad8db30a84809410
Andrew <68841507+andywangyt@users.noreply.github.com>
2021-12-23 13:28:08 -0800

    Time: 0 ms (100.00%), Space: 38.9 MB (71.29%) - LeetHub

--- path-sum/path-sum.java
+++ path-sum/path-sum.java
@@ -13,6 +13,13 @@
  *     }
  * }
  */
+
+/*
+1. If node is not a leaf, one calls recursively hasPathSum method for its children with a sum
+   decreased by the current node value. 
+2. If node is a leaf, checks if the the current sum is zero.
+*/
+    
 class Solution {
   public boolean hasPathSum(TreeNode root, int sum) {
     if (root == null)
