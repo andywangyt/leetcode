05bf295c0f6eaba7972b5540822f1bb0d4e90cc0
Andrew <68841507+andywangyt@users.noreply.github.com>
2022-02-20 23:55:34 -0800

    Time: 10 ms (73.59%), Space: 55 MB (12.50%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.java
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.java
@@ -1,34 +1,27 @@
-class Solution {
-    public int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-    public int rows, columns;
+// DFS + Memoization Solution
+// Accepted and Recommended
+public class Solution {
+    private static final int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    private int m, n;
 
     public int longestIncreasingPath(int[][] matrix) {
-        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
-            return 0;
-        }
-        rows = matrix.length;
-        columns = matrix[0].length;
-        int[][] memo = new int[rows][columns];
+        if (matrix.length == 0) return 0;
+        m = matrix.length; n = matrix[0].length;
+        int[][] cache = new int[m][n];
         int ans = 0;
-        for (int i = 0; i < rows; ++i) {
-            for (int j = 0; j < columns; ++j) {
-                ans = Math.max(ans, dfs(matrix, i, j, memo));
-            }
-        }
+        for (int i = 0; i < m; ++i)
+            for (int j = 0; j < n; ++j)
+                ans = Math.max(ans, dfs(matrix, i, j, cache));
         return ans;
     }
 
-    public int dfs(int[][] matrix, int row, int column, int[][] memo) {
-        if (memo[row][column] != 0) {
-            return memo[row][column];
+    private int dfs(int[][] matrix, int i, int j, int[][] cache) {
+        if (cache[i][j] != 0) return cache[i][j];
+        for (int[] d : dirs) {
+            int x = i + d[0], y = j + d[1];
+            if (0 <= x && x < m && 0 <= y && y < n && matrix[x][y] > matrix[i][j])
+                cache[i][j] = Math.max(cache[i][j], dfs(matrix, x, y, cache));
         }
-        ++memo[row][column];
-        for (int[] dir : dirs) {
-            int newRow = row + dir[0], newColumn = column + dir[1];
-            if (newRow >= 0 && newRow < rows && newColumn >= 0 && newColumn < columns && matrix[newRow][newColumn] > matrix[row][column]) {
-                memo[row][column] = Math.max(memo[row][column], dfs(matrix, newRow, newColumn, memo) + 1);
-            }
-        }
-        return memo[row][column];
+        return ++cache[i][j];
     }
 }
