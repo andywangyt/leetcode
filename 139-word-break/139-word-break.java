8b779e5da18c4c290f6629dc31eb46d8c5dd4e34
Andrew <68841507+andywangyt@users.noreply.github.com>
2022-06-20 20:47:07 -0700

    Time: 4 ms (85.64%), Space: 42.6 MB (78.36%) - LeetHub

--- 139-word-break/139-word-break.java
+++ 139-word-break/139-word-break.java
@@ -1,22 +1,33 @@
 class Solution {
-public boolean wordBreak(String s, List<String> wordDict) {
-        boolean[] visited = new boolean[s.length() + 1];
-        return dfs(s, 0, wordDict, visited);
-    }
-    private boolean dfs(String s, int start, List<String> wordDict, boolean[] visited) {
-        for (String word : wordDict) {
-            int nextStart = start + word.length();
-            if (nextStart > s.length() || visited[nextStart]) {
-                continue;
-            }
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(0);
 
-            if (s.indexOf(word, start) == start) {
-                if (nextStart == s.length() || dfs(s, nextStart, wordDict, visited)) {
-                    return true;
+        int slength = s.length();
+        boolean[] visited = new boolean[slength + 1];
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int start = queue.poll().intValue();
+                for (String word : wordDict) {
+                    int nextStart = start + word.length();
+                    if (nextStart > slength || visited[nextStart]) {
+                        continue;
+                    }
+
+                    if (s.indexOf(word, start) == start) {
+                        if (nextStart == slength) {
+                            return true;
+                        }
+
+                        queue.add(nextStart);
+                        visited[nextStart] = true;
+                    }
                 }
-                visited[nextStart] = true;
             }
         }
+
         return false;
     }
 }
