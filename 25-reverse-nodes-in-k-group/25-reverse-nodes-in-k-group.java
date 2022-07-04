6b1c922f548f78b50c32f4cacad8e27ff8997657
Andrew <68841507+andywangyt@users.noreply.github.com>
2022-07-03 18:57:49 -0700

    Time: 0 ms (100.00%), Space: 45.5 MB (61.69%) - LeetHub

--- 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.java
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.java
@@ -9,43 +9,38 @@
  * }
  */
 class Solution {
-    public ListNode reverseKGroup(ListNode head, int k) {
-        ListNode hair = new ListNode(0);
-        hair.next = head;
-        ListNode pre = hair;
+public ListNode reverseKGroup(ListNode head, int k) {
+    ListNode dummy = new ListNode(0);
+    dummy.next = head;
 
-        while (head != null) {
-            ListNode tail = pre;
-            // 查看剩余部分长度是否大于等于 k
-            for (int i = 0; i < k; ++i) {
-                tail = tail.next;
-                if (tail == null) {
-                    return hair.next;
-                }
-            }
-            ListNode nex = tail.next;
-            ListNode[] reverse = myReverse(head, tail);
-            head = reverse[0];
-            tail = reverse[1];
-            // 把子链表重新接回原链表
-            pre.next = head;
-            tail.next = nex;
-            pre = tail;
-            head = tail.next;
-        }
+    ListNode pre = dummy;
+    ListNode end = dummy;
 
-        return hair.next;
+    while (end.next != null) {
+        for (int i = 0; i < k && end != null; i++) end = end.next;
+        if (end == null) break;
+        ListNode start = pre.next;
+        ListNode next = end.next;
+        end.next = null;
+        pre.next = reverse(start);
+        start.next = next;
+        pre = start;
+
+        end = pre;
     }
+    return dummy.next;
+}
 
-    public ListNode[] myReverse(ListNode head, ListNode tail) {
-        ListNode prev = tail.next;
-        ListNode p = head;
-        while (prev != tail) {
-            ListNode nex = p.next;
-            p.next = prev;
-            prev = p;
-            p = nex;
-        }
-        return new ListNode[]{tail, head};
+private ListNode reverse(ListNode head) {
+    ListNode pre = null;
+    ListNode curr = head;
+    while (curr != null) {
+        ListNode next = curr.next;
+        curr.next = pre;
+        pre = curr;
+        curr = next;
     }
+    return pre;
+}
+
 }
