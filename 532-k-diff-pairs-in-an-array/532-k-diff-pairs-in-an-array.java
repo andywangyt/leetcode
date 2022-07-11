d1002235153c01d3c8d739be66adf7b986b9e61e
Andrew <68841507+andywangyt@users.noreply.github.com>
2022-07-10 23:12:30 -0700

    Time: 19 ms (31.76%), Space: 49.4 MB (14.26%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.java
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.java
@@ -1,23 +1,63 @@
 public class Solution {
-    public int findPairs(int[] nums, int k) {
+//     public int findPairs(int[] nums, int k) {
 
-        int result = 0;
+//         int result = 0;
 
-        HashMap <Integer,Integer> counter = new HashMap<>();
-        for (int n: nums) {
-            counter.put(n, counter.getOrDefault(n, 0)+1);
-        }
+//         HashMap <Integer,Integer> counter = new HashMap<>();
+//         for (int n: nums) {
+//             counter.put(n, counter.getOrDefault(n, 0)+1);
+//         }
 
 
-        for (Map.Entry <Integer, Integer> entry: counter.entrySet()) {
-            int x = entry.getKey();
-            int val = entry.getValue();
-            if (k > 0 && counter.containsKey(x + k)) {
-                result++;
-            } else if (k == 0 && val > 1) {
-                result++;
+//         for (Map.Entry <Integer, Integer> entry: counter.entrySet()) {
+//             int x = entry.getKey();
+//             int val = entry.getValue();
+//             if (k > 0 && counter.containsKey(x + k)) {
+//                 result++;
+//             } else if (k == 0 && val > 1) {
+//                 result++;
+//             }
+//         }
+//         return result;
+//     }
+    
+    
+        public int findPairs(int[] nums, int k) {
+        Set<Integer> visited = new HashSet<Integer>();
+        Set<Integer> res = new HashSet<Integer>();
+        for (int num : nums) {
+            if (visited.contains(num - k)) {
+                res.add(num - k);
             }
+            if (visited.contains(num + k)) {
+                res.add(num);
+            }
+            visited.add(num);
         }
-        return result;
+        return res.size();
     }
+
+
+    
+    
+
+//     public int findPairs(int[] nums, int k) {
+//         Arrays.sort(nums);
+//         int n = nums.length, y = 0, res = 0;
+//         for (int x = 0; x < n; x++) {
+//             if (x == 0 || nums[x] != nums[x - 1]) {
+//                 while (y < n && (nums[y] < nums[x] + k || y <= x)) {
+//                     y++;
+//                 }
+//                 if (y < n && nums[y] == nums[x] + k) {
+//                     res++;
+//                 }
+//             }
+//         }
+//         return res;
+//     }
+    
+    
+    
+    
 }
