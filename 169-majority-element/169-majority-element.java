9b0b42101fe7586142362eca1397e41775efc5ac
Andrew <68841507+andywangyt@users.noreply.github.com>
2022-08-13 00:48:22 -0700

    Time: 332 ms (5.02%), Space: 63.3 MB (6.60%) - LeetHub

---
+++ 169-majority-element/169-majority-element.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+
+       Map<Integer, Integer> counter = new HashMap<>();
+        
+       for (int num: nums) {
+           if (counter.containsKey(num)) {
+              int exist = counter.get(num);  
+              counter.put(num, exist +1);  
+              System.out.println("exists" + exist + " num " + num);
+               
+           }
+           else {
+               System.out.println("npt exists" + " num " + num);
+               counter.put(num,1);   
+           }   
+       } 
+        
+       int half = nums.length / 2;
+       int found = 0; 
+       for (Map.Entry<Integer, Integer> item : counter.entrySet()) {
+           System.out.println("item key" + item.getKey() + " value" +  item.getValue());
+           
+           if (item.getValue() > half) {
+               found = item.getKey();
+               break;
+           }
+           
+       }
+        
+      return found;  
+    }
+}
